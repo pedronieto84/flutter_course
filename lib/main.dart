9e2ed0019d2c4a7c6fa4717a09014a99b0a84a0c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-05 01:49:57 +0200

    Form: Textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,47 @@
 import 'package:flutter/material.dart';
-import 'pages/detail_page.dart';
 
 void main() => runApp(MiApp());
 
 class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dynamic Navigation Example',
-      home: HomePage(),
-      routes: {DetailPage.routeName: (context) => DetailPage()},
-    );
+    return MaterialApp(title: 'Formulario Simple', home: SimpleFormPage());
   }
 }
 
-class HomePage extends StatefulWidget {
+class SimpleFormPage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SimpleFormPage> createState() => _SimpleFormPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SimpleFormPageState extends State<SimpleFormPage> {
   final _formKey = GlobalKey<FormState>();
-  final _idController = TextEditingController();
-
-  @override
-  void dispose() {
-    _idController.dispose();
-    super.dispose();
-  }
-
-  void _navigateToDetail() {
-    if (_formKey.currentState!.validate()) {
-      Navigator.pushNamed(
-        context,
-        DetailPage.routeName,
-        arguments: _idController.text,
-      );
-    }
-  }
+  final TextEditingController _nameController = TextEditingController();
+ 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Enter ID')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _idController,
-                decoration: InputDecoration(labelText: 'Enter ID'),
-                validator: (value) => value == null || value.isEmpty
-                    ? 'Please enter an ID'
-                    : null,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _navigateToDetail,
-                child: Text('Go to Detail Page'),
-              ),
-            ],
+      appBar: AppBar(title: Text('Formulario Simple')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Nombre',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+           
+              ],
+            ),
           ),
         ),
       ),
