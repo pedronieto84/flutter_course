f96f810a03e01124a119a8438d9b4d99f9c6de8b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-04 19:50:59 +0200

    Dart: Funcions

--- main.dart
+++ main.dart
@@ -1,8 +1,39 @@
 // Dart: Funciones
 void main() {
- saludar();
+  saludar();
+  saludarPersona('Juan');
+  int resultado = sumar(5, 3);
+  print('Resultado de la suma: $resultado');
+  int producto = multiplicar(4, 2);
+  print('Producto: $producto');
+  mostrarMensaje('¡Hola, Dart!', 'Even');
+  imprimirDatos(nombre: 'Luis');
 }
 
+// Sin parametros
 void saludar() {
   print('Hola, mundo!');
+}
+
+// Con parametros
+void saludarPersona(String nombre) {
+  print('Hola, $nombre!');
+}
+
+// Con retorno
+int sumar(int a, int b) {
+  return a + b;
+}
+
+// Arrow function solamente si es una sola línea
+int multiplicar(int x, int y) => x * y;
+
+// Con parámetros opcionales por defecto
+void mostrarMensaje(String mensaje, [String autor = 'Anónimo']) {
+  print('"$mensaje" - $autor');
+}
+
+// Con parámetros nombrados
+void imprimirDatos({required String nombre, int edad = 0}) {
+  print('Nombre: $nombre, Edad: $edad');
 }
