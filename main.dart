0432f7ec8593555d356a7dfcd9c335fdec4dec5c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-04 11:20:17 +0200

    While y do-while

--- main.dart
+++ main.dart
@@ -1,9 +1,18 @@
-// Bucle for
+// Bucle while y do-while
 
 void main() {
-  // Bucle for clásico
-  for (int i = 0; i < 5; i++) {
-    print("Iteración $i");
+
+  // WHILE LOOP
+  int i = 0;
+  while (i < 3) {
+    print("while: $i");
+    i++;
   }
 
+  // DO WHILE
+  int j = 0;
+  do {
+    print("do-while: $j");
+    j++;
+  } while (j < 0);
 }
