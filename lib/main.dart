2bcc19da869629c910bcadfb41e80e06f5d186aa
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-04 19:15:44 +0200

    StatefullWidget: Exemple de app que canvia el texte al clicar un botó.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MiApp(),
-  );
+  runApp(MiApp());
 }
 
 class MiApp extends StatelessWidget {
@@ -12,13 +10,46 @@ class MiApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            'Hola Flutter'
-          ),
+          child: TextoDinamico(),
         ),
       ),
     );
   }
 }
 
+class TextoDinamico extends StatefulWidget {
+  @override
+  State<TextoDinamico> createState() => _TextoDinamicoState();
+}
 
+class _TextoDinamicoState extends State<TextoDinamico> {
+  String mensaje = 'Hola Flutter';
+
+  void cambiarTexto() {
+    setState(() {
+      mensaje = 'Has pulsado el botón';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          mensaje,
+          style: const TextStyle(
+            fontFamily: 'Arial',
+            fontSize: 25,
+            color: Colors.red,
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: cambiarTexto,
+          child: const Text('Cambiar texto'),
+        ),
+      ],
+    );
+  }
+}
