63b40660e6fbbcd33186f065f5af7ef1899a11e9
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-04 18:32:13 +0200

    Text: cambiamos color del texto, tamaño y formato de la letra.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hola Mundo'))),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hola Flutter',
+            style: TextStyle(
+              fontFamily: 'Arial',
+              fontSize: 15,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
