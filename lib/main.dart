0c0e7a1301bb21da40469b2a3f8345fe0c70b4d5
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-05 14:57:46 +0200

    Widget Personalizado: Ejemplo botón

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,24 @@
 import 'package:flutter/material.dart';
-import 'pages/pagina_containers.dart';
-import 'pages/pagina_column_row.dart';
-import 'pages/pagina_stack.dart';
-import 'pages/pagina_gridview.dart';
-import 'pages/pagina_table.dart';
 
-void main() => runApp(MiApp());
 
-class MiApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Ejemplo de Layouts', home: MenuNavegacion());
-  }
-}
+class MiBoton extends StatelessWidget {
+  final String texto;
 
-class MenuNavegacion extends StatefulWidget {
-  @override
-  _MenuNavegacionState createState() => _MenuNavegacionState();
-}
-
-class _MenuNavegacionState extends State<MenuNavegacion> {
-  int _paginaActual = 0;
-
-  final List<Widget> _paginas = const [
-    PaginaContainers(),
-    PaginaColumnRow(),
-    PaginaStack(),
-    PaginaGridView(),
-    PaginaTable(),
-  ];
+  MiBoton({required this.texto});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 125, 213, 234), // Fondo claro para toda la app
-      body: IndexedStack(index: _paginaActual, children: _paginas),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.purple, // Fondo oscuro para el menú
-        selectedItemColor: Colors.black, // Ítem seleccionado en ámbar
-        unselectedItemColor:
-            Colors.blueAccent, // Ítems no seleccionados en azul
-        currentIndex: _paginaActual,
-        onTap: (index) {
-          setState(() {
-            _paginaActual = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.crop_square),
-            label: 'Containers',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.view_column),
-            label: 'Column/Row',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.layers), label: 'Stack'),
-          BottomNavigationBarItem(icon: Icon(Icons.grid_on), label: 'GridView'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.table_chart),
-            label: 'Table',
-          ),
-        ],
-      ),
+    return ElevatedButton(
+      onPressed: () => print("Pulsado"),
+      child: Text(texto),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(child: MiBoton(texto: 'Haz clic')),
+    ),
+  ));
 }
