ba6b8d439ea3fa66ee01ddfbd3aee785bd17e8d9
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-05 16:03:30 +0200

    Comunicación entre Widgets: ENTRE HERMANOS CON PROVIDER

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MiApp());
+void main() => runApp(
+  ChangeNotifierProvider(create: (_) => MensajeGlobal(), child: MiApp()),
+);
+
+class MensajeGlobal extends ChangeNotifier {
+  String mensajeA = 'Sin mensaje de B';
+  String mensajeB = 'Sin mensaje de A';
+
+  void enviarDeA(String msg) {
+    mensajeB = msg;
+    notifyListeners();
+  }
+
+  void enviarDeB(String msg) {
+    mensajeA = msg;
+    notifyListeners();
+  }
+}
 
 class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Comunicación entre Hermanos Bidireccional',
+      title: 'Provider: Comunicación Global',
       home: Scaffold(
-        appBar: AppBar(title: Text('Hermanos: Mensajes en ambas direcciones')),
-        body: Center(child: Padre()),
-      ),
-    );
-  }
-}
-
-class Padre extends StatefulWidget {
-  @override
-  State<Padre> createState() => _PadreState();
-}
-
-class _PadreState extends State<Padre> {
-  String mensajeA = 'Sin mensaje de B';
-  String mensajeB = 'Sin mensaje de A';
-
-  void actualizarDesdeA(String nuevoMensaje) {
-    setState(() {
-      mensajeB = nuevoMensaje;
-    });
-  }
-
-  void actualizarDesdeB(String nuevoMensaje) {
-    setState(() {
-      mensajeA = nuevoMensaje;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Colors.lightBlue[50],
-        border: Border.all(color: Colors.blue, width: 3),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Padre',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        appBar: AppBar(title: Text('Provider: Hermanos')),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [HijoA(), SizedBox(height: 20), HijoB()],
           ),
-          SizedBox(height: 20),
-          HijoA(onEnviar: actualizarDesdeA, mensaje: mensajeA),
-          SizedBox(height: 20),
-          HijoB(onEnviar: actualizarDesdeB, mensaje: mensajeB),
-        ],
+        ),
       ),
     );
   }
 }
 
 class HijoA extends StatelessWidget {
-  final void Function(String) onEnviar;
-  final String mensaje;
-  const HijoA({required this.onEnviar, required this.mensaje});
-
   @override
   Widget build(BuildContext context) {
+    final global = Provider.of<MensajeGlobal>(context);
     return Container(
       padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
@@ -86,11 +58,14 @@ class HijoA extends StatelessWidget {
           ),
           SizedBox(height: 10),
           ElevatedButton(
-            onPressed: () => onEnviar('¡Mensaje de A para B!'),
+            onPressed: () => global.enviarDeA('¡Mensaje de A para B!'),
             child: Text('Enviar a Hijo B'),
           ),
           SizedBox(height: 10),
-          Text('Mensaje de B: $mensaje', style: TextStyle(fontSize: 16)),
+          Text(
+            'Mensaje de B: ${global.mensajeA}',
+            style: TextStyle(fontSize: 16),
+          ),
         ],
       ),
     );
@@ -98,12 +73,9 @@ class HijoA extends StatelessWidget {
 }
 
 class HijoB extends StatelessWidget {
-  final void Function(String) onEnviar;
-  final String mensaje;
-  const HijoB({required this.onEnviar, required this.mensaje});
-
   @override
   Widget build(BuildContext context) {
+    final global = Provider.of<MensajeGlobal>(context);
     return Container(
       padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
@@ -120,11 +92,14 @@ class HijoB extends StatelessWidget {
           ),
           SizedBox(height: 10),
           ElevatedButton(
-            onPressed: () => onEnviar('¡Mensaje de B para A!'),
+            onPressed: () => global.enviarDeB('¡Mensaje de B para A!'),
             child: Text('Enviar a Hijo A'),
           ),
           SizedBox(height: 10),
-          Text('Mensaje de A: $mensaje', style: TextStyle(fontSize: 16)),
+          Text(
+            'Mensaje de A: ${global.mensajeB}',
+            style: TextStyle(fontSize: 16),
+          ),
         ],
       ),
     );
